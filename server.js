4d9c26bfbca7476c7e90c83818da8b72ac1743e5
alvarodev12 <alsouza23@outlook.com>
2025-07-01 13:45:45 -0300

    feat: adding and saving user in database

--- server.js
+++ server.js
@@ -1,18 +1,29 @@
 import express from "express";
+import { PrismaClient } from "./generated/prisma/index.js";
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 
 const users = [];
 
-app.get("/usuarios", (req, res) => {
+app.get("/usuarios", async (req, res) => {
+  const users = await prisma.user.findMany();
+
   res.status(200).json(users);
 });
 
-app.post("/usuarios", (req, res) => {
-  users.push(req.body);
+app.post("/usuarios", async (req, res) => {
+  const user = await prisma.user.create({
+    data: {
+      email: req.body.email,
+      age: req.body.age,
+      name: req.body.name,
+    },
+  });
 
-  res.status(201).json({ message: "Usuários criado com sucesso" });
+  res.status(201).json(user);
 });
 
 app.listen(3000);
