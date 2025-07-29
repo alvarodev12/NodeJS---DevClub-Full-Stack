bed763c4dafdc6463b24a8c41a595c0ad86d2095
alvarodev12 <alsouza23@outlook.com>
2025-07-29 19:49:06 -0300

    feat: instalação cors, conectando backend com frontend

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { PrismaClient } from "./generated/prisma/index.js";
+import cors from "cors";
 
 const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const users = [];
 
